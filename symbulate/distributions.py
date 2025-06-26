e96896040670f3cff7983546c6f24413e0d2aa21
kevindavisross <kjross@calpoly.edu>
2025-06-26 13:39:32 -0700

    Fix re: "Follow-up on issue #92 : seaborn-blind error" on main branch

--- symbulate/distributions.py
+++ symbulate/distributions.py
@@ -91,7 +91,7 @@ class Distribution(ProbabilitySpace):
         ax.set_ylim(*ylim)
         
         # get next color in cycle
-        color = get_next_color(ax)
+        color = get_next_color()
 
         # plot points for discrete distributions
         if self.discrete and not cdf:
